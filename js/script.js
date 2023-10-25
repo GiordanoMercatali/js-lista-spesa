7b9203506a77259a17e1374358c288e569d8377a
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-25 18:07:16 +0200

    add: you can now add your own items

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-const shoppingList = ["mozzarella", "fish", "pasta", "ketchup"];
+// const shoppingList = ["mozzarella", "fish", "pasta", "ketchup"];
+const shoppingList = [];
 console.log(shoppingList);
 
 let listString = "";
 let i = 0;
+let j = 0;
+let listLength;
 
-while (i < shoppingList.length){
-    const curItem = shoppingList[i];
-    listString += `<li>${curItem}</li>`;
-    i++;
+do{
+    listLength = parseInt(prompt("How many items do you have to buy?"));
+    if(!isNaN(listLength) && listLength != 0){
+        j++;
+    }
+
+}while(j < 1)
+console.log(listLength);
+
+// while (i < shoppingList.length){
+    while (i < listLength){
+        let curItem = prompt("Which items do you have to buy?");
+        // const curItem = shoppingList[i];
+        listString += `<li>${curItem}</li>`;
+        i++;
 }
 
 document.querySelector("ul").innerHTML = listString;
