454e629f10a82bc6c4b91966eb3810ffd7de7e89
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-25 17:52:29 +0200

    add: list print on screen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+const shoppingList = ["mozzarella", "fish", "pasta", "ketchup"];
+console.log(shoppingList);
 
+let listString = "";
+let i = 0;
 
+while (i < shoppingList.length){
+    const curItem = shoppingList[i];
+    listString += `<li>${curItem}</li>`;
+    i++;
+}
 
-
+document.querySelector("ul").innerHTML = listString;
